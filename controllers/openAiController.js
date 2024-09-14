6e5a0112faf74c1d99efb78f23edb1932f95aff9
Muntajir Hussain Gazi <muntajirwork11@gmail.com>
2024-09-14 11:42:32 +0530

    setup routes and controllers
    Backend setup complete
    
    Co-Authored-By: Sayantan das <92078956+321sayantan@users.noreply.github.com>

---
+++ controllers/openAiController.js
@@ -0,0 +1,20 @@
+export const summaryController = (req, res) => {
+    res.json({ message: "Summary Controller Dummy Response" });
+  };
+  
+  export const paragraphController = (req, res) => {
+    res.json({ message: "Paragraph Controller Dummy Response" });
+  };
+  
+  export const chatbotController = (req, res) => {
+    res.json({ message: "Chatbot Controller Dummy Response" });
+  };
+  
+  export const jsconverterController = (req, res) => {
+    res.json({ message: "JS Converter Controller Dummy Response" });
+  };
+  
+  export const scifiImageController = (req, res) => {
+    res.json({ message: "Sci-fi Image Controller Dummy Response" });
+  };
+  
---
+++ routes/openaiRoutes.js
@@ -0,0 +1,19 @@
+import express from "express";
+import {
+  summaryController,
+  paragraphController,
+  chatbotController,
+  jsconverterController,
+  scifiImageController,
+} from "../controllers/openAiController.js"; 
+
+const router = express.Router();
+
+// routes
+router.post("/summary", summaryController);
+router.post("/paragraph", paragraphController);
+router.post("/chatbot", chatbotController);
+router.post("/js-converter", jsconverterController);
+router.post("/scifi-image", scifiImageController);
+
+export default router;
