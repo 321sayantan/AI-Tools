9081e1ff3b6065573193d8244b055e2c0e152685
Muntajir Hussain Gazi <muntajirwork11@gmail.com>
2024-09-18 19:48:57 +0530

    route typo fix

---
+++ routes/genAiRoutes.js
@@ -0,0 +1,19 @@
+import express from "express";
+import {
+  summaryController,
+  paragraphController,
+  chatbotController,
+  jsconverterController,
+  scifiImageController,
+} from "../controllers/genAiRoutes.js"; 
+
+const router = express.Router();
+
+
+router.post("/summary", summaryController);
+router.post("/paragraph", paragraphController);
+router.post("/chatbot", chatbotController);
+router.post("/js-converter", jsconverterController);
+router.post("/scifi-image", scifiImageController);
+
+export default router;
