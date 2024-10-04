5dbc3010dfdfecbe68556265e031009f4f0f6662
sayantan <123sayantandas@gmail.com>
2024-10-04 17:36:12 +0530

    updating

--- client/src/pages/LogIn.jsx
+++ client/src/pages/LogIn.jsx
@@ -13,6 +13,21 @@ import {
   Collapse,
 } from "@mui/material";
 
+import "../css/login.css";
+
+import {
+  MDBBtn,
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBCard,
+  MDBCardBody,
+  MDBInput,
+  MDBCheckbox,
+  MDBIcon,
+} from "mdb-react-ui-kit";
+
+
 const LogIn = () => {
   const theme = useTheme();
   const navigate = useNavigate();
@@ -28,10 +43,12 @@ const LogIn = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const { data } = await axios.post("http://localhost:5000/api/v1/auth/LogIn", { email, password });
-      console.log(1,data.token.accessToken)
-      if(data.token.accessToken)
-      {
+      const { data } = await axios.post(
+        "http://localhost:5000/api/v1/auth/LogIn",
+        { email, password }
+      );
+      console.log(1, data.token.accessToken);
+      if (data.token.accessToken) {
         localStorage.setItem("authToken", true);
         toast.success("LogIn Successful");
         navigate("/");
@@ -48,59 +65,192 @@ const LogIn = () => {
       }, 5000);
     }
   };
-  return (
-    <Box
-      width={isNotMobile ? "40%" : "80%"}
-      p={"2rem"}
-      m={"2rem auto"}
-      borderRadius={5}
-      sx={{ boxShadow: 5 }}
-      backgroundColor={theme.palette.background.alt}
-    >
-      <Collapse in={error}>
-        <Alert severity="error" sx={{ mb: 2 }}>
-          {error}
-        </Alert>
-      </Collapse>
-      <form onSubmit={handleSubmit}>
-        <Typography variant="h3">Sign in</Typography>
-        <TextField
-          label="email"
-          type="email"
-          required
-          margin="normal"
-          fullWidth
-          value={email}
-          onChange={(e) => {
-            setEmail(e.target.value);
-          }}
-        />
-        <TextField
-          label="password"
-          type="password"
-          required
-          margin="normal"
-          fullWidth
-          value={password}
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
-        />
-        <Button
-          type="submit"
-          fullWidth
-          variant="contained"
-          size="large"
-          sx={{ color: "white", mt: 2 }}
-        >
-          Sign Up
-        </Button>
-        <Typography mt={2}>
-          Don't have an account ? <Link to="/register">Please Register</Link>
-        </Typography>
-      </form>
-    </Box>
-  );
+  return function App() {
+    return (
+      <MDBContainer
+        fluid
+        className="p-4 background-radial-gradient overflow-hidden"
+      >a
+        <MDBRow>
+          <MDBCol
+            md="6"
+            className="text-center text-md-start d-flex flex-column justify-content-center"
+          >
+            <h1
+              className="my-5 display-3 fw-bold ls-tight px-3"
+              style={{ color: "hsl(218, 81%, 95%)" }}
+            >
+              The best offer <br />
+              <span style={{ color: "hsl(218, 81%, 75%)" }}>
+                for your business
+              </span>
+            </h1>
+
+            <p className="px-3" style={{ color: "hsl(218, 81%, 85%)" }}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet,
+              itaque accusantium odio, soluta, corrupti aliquam quibusdam
+              tempora at cupiditate quis eum maiores libero veritatis? Dicta
+              facilis sint aliquid ipsum atque?
+            </p>
+          </MDBCol>
+
+          <MDBCol md="6" className="position-relative">
+            <div
+              id="radius-shape-1"
+              className="position-absolute rounded-circle shadow-5-strong"
+            ></div>
+            <div
+              id="radius-shape-2"
+              className="position-absolute shadow-5-strong"
+            ></div>
+
+            <MDBCard className="my-5 bg-glass">
+              <MDBCardBody className="p-5">
+                <MDBRow>
+                  <MDBCol col="6">
+                    <MDBInput
+                      wrapperClass="mb-4"
+                      label="First name"
+                      id="form1"
+                      type="text"
+                    />
+                  </MDBCol>
+
+                  <MDBCol col="6">
+                    <MDBInput
+                      wrapperClass="mb-4"
+                      label="Last name"
+                      id="form2"
+                      type="text"
+                    />
+                  </MDBCol>
+                </MDBRow>
+
+                <MDBInput
+                  wrapperClass="mb-4"
+                  label="Email"
+                  id="form3"
+                  type="email"
+                />
+                <MDBInput
+                  wrapperClass="mb-4"
+                  label="Password"
+                  id="form4"
+                  type="password"
+                />
+
+                <div className="d-flex justify-content-center mb-4">
+                  <MDBCheckbox
+                    name="flexCheck"
+                    value=""
+                    id="flexCheckDefault"
+                    label="Subscribe to our newsletter"
+                  />
+                </div>
+
+                <MDBBtn className="w-100 mb-4" size="md">
+                  sign up
+                </MDBBtn>
+
+                <div className="text-center">
+                  <p>or sign up with:</p>
+
+                  <MDBBtn
+                    tag="a"
+                    color="none"
+                    className="mx-3"
+                    style={{ color: "#1266f1" }}
+                  >
+                    <MDBIcon fab icon="facebook-f" size="sm" />
+                  </MDBBtn>
+
+                  <MDBBtn
+                    tag="a"
+                    color="none"
+                    className="mx-3"
+                    style={{ color: "#1266f1" }}
+                  >
+                    <MDBIcon fab icon="twitter" size="sm" />
+                  </MDBBtn>
+
+                  <MDBBtn
+                    tag="a"
+                    color="none"
+                    className="mx-3"
+                    style={{ color: "#1266f1" }}
+                  >
+                    <MDBIcon fab icon="google" size="sm" />
+                  </MDBBtn>
+
+                  <MDBBtn
+                    tag="a"
+                    color="none"
+                    className="mx-3"
+                    style={{ color: "#1266f1" }}
+                  >
+                    <MDBIcon fab icon="github" size="sm" />
+                  </MDBBtn>
+                </div>
+              </MDBCardBody>
+            </MDBCard>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+    );
+  };
 };
 
 export default LogIn;
+
+// <Box
+//   width={isNotMobile ? "40%" : "80%"}
+//   p={"2rem"}
+//   m={"2rem auto"}
+//   borderRadius={5}
+//   sx={{ boxShadow: 5 }}
+//   backgroundColor={theme.palette.background.alt}
+//   className="mt-5"
+// >
+//   <Collapse in={error}>
+//     <Alert severity="error" sx={{ mb: 2 }}>
+//       {error}
+//     </Alert>
+//   </Collapse>
+//   <form onSubmit={handleSubmit}>
+//     <Typography variant="h3">Sign in</Typography>
+//     <TextField
+//       label="email"
+//       type="email"
+//       required
+//       margin="normal"
+//       fullWidth
+//       value={email}
+//       onChange={(e) => {
+//         setEmail(e.target.value);
+//       }}
+//     />
+//     <TextField
+//       label="password"
+//       type="password"
+//       required
+//       margin="normal"
+//       fullWidth
+//       value={password}
+//       onChange={(e) => {
+//         setPassword(e.target.value);
+//       }}
+//     />
+//     <Button
+//       type="submit"
+//       fullWidth
+//       variant="contained"
+//       size="large"
+//       sx={{ color: "white", mt: 2 }}
+//     >
+//       Sign Up
+//     </Button>
+//     <Typography mt={2}>
+//       Don't have an account ? <Link to="/register">Please Register</Link>
+//     </Typography>
+//   </form>
+// </Box>;
