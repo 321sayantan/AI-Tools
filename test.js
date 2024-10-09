4e54fd4052c9d66f7f437582d5987caeccc68b5c
sayantan <123sayantandas@gmail.com>
2024-10-09 19:52:49 +0530

    chatbot backend test

--- test.js
+++ test.js
@@ -1,10 +1,20 @@
 // Make sure to include these imports:
-
-import { GoogleGenerativeAI } from "@google/generative-ai";
+// import { GoogleGenerativeAI } from "@google/generative-ai";
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-
-const prompt = "Write a story about a magic backpack.";
-
-const result = await model.generateContent(prompt);
+const chat = model.startChat({
+  history: [
+    {
+      role: "user",
+      parts: [{ text: "Hello" }],
+    },
+    {
+      role: "model",
+      parts: [{ text: "Great to meet you. What would you like to know?" }],
+    },
+  ],
+});
+let result = await chat.sendMessage("I have 2 dogs in my house.");
+console.log(result.response.text());
+result = await chat.sendMessage("How many paws are in my house?");
 console.log(result.response.text());
